7da9dfe9a4167dbceb29dfb28aa202fc3a371b53
bryanahusna <51874424+bryanahusna@users.noreply.github.com>
2022-10-23 13:08:22 +0700

    feat: add songDetail, albumDetail API

--- dummy/insert_dummy.sql
+++ dummy/insert_dummy.sql
@@ -1,2 +1,5 @@
-INSERT INTO song(judul, penyanyi, tanggal_terbit, genre, duration, audio_path)
-    VALUES('Erika', 'Karl', '2022-10-22', 'folk', 92, '/media/erika/erika.mp3');
+INSERT INTO album(judul, penyanyi, total_duration, image_path, tanggal_terbit)
+    VALUES ('Erika Album', 'Karl', 92, '/media/erika-album/erika-album.jpg', '2022-10-22');
+
+INSERT INTO song(judul, penyanyi, tanggal_terbit, genre, duration, audio_path, album_id)
+    VALUES('Erika', 'Karl', '2022-10-22', 'folk', 92, '/media/erika/erika.mp3', 1);
---
+++ src/config.php
@@ -0,0 +1,9 @@
+<?php
+    return array(
+        'db_host' => 'localhost',
+        'db_database' => 'binotify',
+        'db_user' => 'root',
+        'db_password' => 'bryankeren',
+        'db_pdo_connect' => "mysql:host=localhost;dbname=binotify"
+    );
+?>
