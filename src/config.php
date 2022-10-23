9812cc15819ca28dcf27e9eff6c5a4c0ca8730ee
bryanahusna <51874424+bryanahusna@users.noreply.github.com>
2022-10-23 14:46:55 +0700

    feat: add search and searchPagesCount API

--- src/config.php
+++ src/config.php
@@ -3,7 +3,9 @@
         'db_host' => 'localhost',
         'db_database' => 'binotify',
         'db_user' => 'root',
-        'db_password' => 'bryankeren',
-        'db_pdo_connect' => "mysql:host=localhost;dbname=binotify"
+        'db_password' => '',
+        'db_pdo_connect' => "mysql:host=localhost;dbname=binotify",
+
+        'search_defaultpagesize' => 10
     );
 ?>
