5a89996933fe5ce5a9513588ccc49871ae1d6056
Mahesa Lizardy <88304455+lizardyy@users.noreply.github.com>
2022-10-25 17:04:15 +0700

    feat: add navbar component and logo

---
+++ src/navbar/routing.js
@@ -0,0 +1,20 @@
+const route = (event) => {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    handleLocation();
+};
+
+const routes = {
+    404 : "",
+    "/album": "/album",
+};
+
+const handleLocation = async()=>{
+    const path = window.location.pathname
+    const route = routes[path] || routes[404]
+    const html = await fetch(route).then((data)=>data.text())
+    document.getElementById("page").innerHTML = html;
+}
+window.ponpopstate = handleLocation;
+window.route = route;
