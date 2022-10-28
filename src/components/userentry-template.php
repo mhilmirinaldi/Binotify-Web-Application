cce0cf413ef712babca9fd06fc8927d9604e2838
Mahesa Lizardy <88304455+lizardyy@users.noreply.github.com>
2022-10-28 13:45:43 +0700

    fix: fix list user

--- src/components/userentry-template.php
+++ src/components/userentry-template.php
@@ -1,5 +1,5 @@
 <?php
-$i = 0;
+$i = 1;
 
 function generateUser($user){
     global $i;
@@ -9,13 +9,21 @@ function generateUser($user){
         $admin = "User";
     }
     
-    if($i == 0){
+    if($i == 1){
         $html = <<<EOF
         <div class="user">
             <div class="number">No.</div>
             <div class="user-email">Email</div>
             <div class="user-username">Username</div>
             <div class="user-role">Role</div>
+        </div>
+        <link href="/components/userentry-template.css" rel="stylesheet">
+        
+        <div class="user">
+                <div class="number">{$i}</div>  
+                <div class="user-email">{$user['email']}</div>
+                <div class="user-username">{$user['username']}</div>
+                <div class="user-role">{$admin}</div>
         </div>
         EOF;
     } else {
